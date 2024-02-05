9b85a893220d33437ec2331f081cf74d8ac7b0d4
priyank ladumor <137873635+priyank-ladumor@users.noreply.github.com>
2024-02-05 19:00:31 +0530

    pagination in get all categories lvl

--- server/src/controllers/categories.controllers.js
+++ server/src/controllers/categories.controllers.js
@@ -2,7 +2,7 @@ import { getTopLevelCategory, getThirdLevelCategory, getSecondLevelCategory } fr
 
 export const getTopLevelCategoryController = async (req, res) => {
     try {
-        const topLevel = await getTopLevelCategory();
+        const topLevel = await getTopLevelCategory(req);
         return res.status(200).send(topLevel);
     } catch (error) {
         return res.status(500).send({ error: error.message })
@@ -11,7 +11,7 @@ export const getTopLevelCategoryController = async (req, res) => {
 
 export const getSecondLevelCategoryController = async (req, res) => {
     try {
-        const secondLevel = await getSecondLevelCategory();
+        const secondLevel = await getSecondLevelCategory(req);
         return res.status(200).send(secondLevel);
     } catch (error) {
         return res.status(500).send({ error: error.message })
@@ -20,7 +20,7 @@ export const getSecondLevelCategoryController = async (req, res) => {
 
 export const getThirdLevelCategoryController = async (req, res) => {
     try {
-        const thirdLevel = await getThirdLevelCategory();
+        const thirdLevel = await getThirdLevelCategory(req);
         return res.status(200).send(thirdLevel);
     } catch (error) {
         return res.status(500).send({ error: error.message })
--- server/src/services/categories.service.js
+++ server/src/services/categories.service.js
@@ -1,28 +1,44 @@
 import { Categories } from "../models/category.model.js"
 
 
-export const getTopLevelCategory = async () => {
-    const level1 = await Categories.find({level: 1})
+export const getTopLevelCategory = async (reqData) => {
+    const level1 = Categories.find({ level: 1 })
     if (level1) {
-        return level1;
+        const { pageNumber, pageSize } = reqData.query;
+        const totalLv1 = await Categories.countDocuments(level1);
+        const AllLv1 = await level1.skip((pageNumber - 1) * pageSize).limit(pageSize);
+        const totalPages = Math.ceil(totalLv1 / pageSize);
+
+        return { content: AllLv1, currentPage: pageNumber, totalPages: totalPages }
     } else {
         throw new Error("no available top level category")
     }
 }
 
-export const getSecondLevelCategory = async () => {
-    const level2 = await Categories.find({level: 2})
+export const getSecondLevelCategory = async (reqData) => {
+    const level2 = Categories.find({ level: 2 })
     if (level2) {
-        return level2;
+        const { pageNumber, pageSize } = reqData.query;
+        const totalLv2 = await Categories.countDocuments(level2);
+        const AllLv2 = await level2.skip((pageNumber - 1) * pageSize).limit(pageSize);
+        const totalPages = Math.ceil(totalLv2 / pageSize);
+
+        return { content: AllLv2, currentPage: pageNumber, totalPages: totalPages }
+
     } else {
         throw new Error("no available second level category")
     }
 }
 
-export const getThirdLevelCategory = async () => {
-    const level3 = await Categories.find({level: 3})
+export const getThirdLevelCategory = async (reqData) => {
+    const level3 = Categories.find({ level: 3 })
     if (level3) {
-        return level3;
+        const { pageNumber, pageSize } = reqData.query;
+        const totalLv3 = await Categories.countDocuments(level3);
+        const AllLv3 = await level3.skip((pageNumber - 1) * pageSize).limit(pageSize);
+        const totalPages = Math.ceil(totalLv3 / pageSize);
+
+        return { content: AllLv3, currentPage: pageNumber, totalPages: totalPages }
     } else {
         throw new Error("no available third level category")
     }
