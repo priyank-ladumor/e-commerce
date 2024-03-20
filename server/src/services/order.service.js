7c38651bade503402d3a114e52cd9779b0292938
priyank ladumor <137873635+priyank-ladumor@users.noreply.github.com>
2024-03-20 13:02:11 +0530

    quantity check func added in create order api and solved that api major bugs

--- server/src/services/order.service.js
+++ server/src/services/order.service.js
@@ -11,20 +11,27 @@ import { findProductById } from "./product.service.js"
 export const checkProductQuantityAvailable = async (body) => {
     const { pid, quantity, color, size } = body;
     if (pid && quantity && color && size) {
-        const product = await Product.findOne({ _id: pid });
-        const matchSizeAndColor = product.sizesAndColor.filter((ele) => ele.size === size && ele.color === color)
-        if (matchSizeAndColor[0].quantity >= quantity) {
+        const product = await Product?.findOne({ _id: pid });
+        const matchSizeAndColor = product && product?.sizesAndColor.filter((ele) => ele.size === size && ele.color === color)
+        if (matchSizeAndColor[0]?.quantity >= quantity) {
             return "quantity available"
         } else {
-            throw new Error(`This size: ${size} and title: ${product.title} has ${matchSizeAndColor[0].quantity} quantity left`);
+            return { msg: `This size: ${size} and title: ${product.title} has ${matchSizeAndColor[0].quantity} quantity left` }
         }
     } else {
         throw new Error('body data not received');
     }
 }
 
+
 export const createOrder = async (user, body) => {
-    const { selectedAddress, paymentSys, cartId } = body;
+    const { selectedAddress, paymentSys, cartId, cartItemDetails } = body;
+
+    if (cartItemDetails?.length === 0) {
+        throw new Error("cart items not found");
+    }
+    //for validate quantity available in product or not
+    const validate = cartItemDetails && cartItemDetails?.map((ele) => checkProductQuantityAvailable(ele));
     const address = await Address.findById(selectedAddress);
 
     let cart = await Cart.findById({ _id: cartId }).populate({
@@ -32,59 +39,70 @@ export const createOrder = async (user, body) => {
             path: "product", model: Product
         }
     })
+
     const OrderItems = [];
+    let qunatityNotAvailable = [];
+    const validationResults = await Promise.all(validate);
+    const isValid = validationResults && validationResults?.map((ele) => ele.msg?.length > 0 && qunatityNotAvailable.push(ele.msg))
 
-    for (const item of cart.cartItem) {
-        const orderItemCreate = new OrderItem({
-            product: item.product,
-            user: item.user,
-            size: item.size,
-            color: item.color,
-            quantity: item.quantity,
-            price: item.price
+    if (qunatityNotAvailable[0]) {
+        throw new Error(qunatityNotAvailable[0]);
+    } else {
+        for (const item of cart.cartItem) {
+            const orderItemCreate = new OrderItem({
+                product: item.product,
+                user: item.user,
+                size: item.size,
+                color: item.color,
+                quantity: item.quantity,
+                price: item.price
+            })
+
+            const createdOrderItem = await orderItemCreate.save();
+            await OrderItems.push(createdOrderItem);
+        }
+
+        const createdOrder = new Order({
+            shippingAddress: address,
+            totalPrice: cart.totalPrice,
+            orderItem: OrderItems,
+            totalItem: cart.totalItem,
+            user: cart.user,
         })
+        createdOrder.paymentDetails.paymentMethod = paymentSys;
+        const savedOrder = await createdOrder.save();
 
-        const createdOrderItem = await orderItemCreate.save();
-        await OrderItems.push(createdOrderItem);
+        const delCartItem = cart?.cartItem?.map((ele) => ele._id);
+        for (let id of delCartItem) {
+            const findCartItem = await findCartItemById(id);
+            const product = await findProductById(findCartItem?.product[0]?._id)
+            product.quantity = product.quantity - findCartItem?.quantity;
+            const updateQuantityId = product.sizesAndColor?.filter((ele) =>
+                ele.size === findCartItem?.size && ele.color === findCartItem?.color
+            )
+            const QuantityUpdate =
+                //for updating product quantity
+                await Product.updateOne({
+                    _id: findCartItem?.product[0]?._id,
+                    "sizesAndColor._id": updateQuantityId[0]?._id,
+                }, {
+                    $set: {
+                        'sizesAndColor.$.quantity': updateQuantityId[0]?.quantity - findCartItem?.quantity,
+                    }
+                })
+            await product.save();
+            //for  delete the Cart Item from User's Shopping
+            const del = await deleteCartItem(id)
+        }
+        //for delete the user cart
+        await Cart.findByIdAndDelete(cartId)
+
+        if (savedOrder) {
+            return "Order Created Successfully";
+        }
     }
 
-    const createdOrder = new Order({
-        shippingAddress: address,
-        totalPrice: cart.totalPrice,
-        orderItem: OrderItems,
-        totalItem: cart.totalItem,
-        user: cart.user,
-    })
-    createdOrder.paymentDetails.paymentMethod = paymentSys;
-    const savedOrder = await createdOrder.save();
 
-    // const delCartItem = cart?.cartItem?.map((ele) => ele._id);
-    // for (let id of delCartItem) {
-    //     const findCartItem = await findCartItemById(id);
-    //     const product = await findProductById(findCartItem?.product[0]?._id)
-    //     product.quantity = product.quantity - findCartItem?.quantity;
-    //     const updateQuantityId = product.sizesAndColor?.filter((ele) =>
-    //         ele.size === findCartItem?.size && ele.color === findCartItem?.color
-    //     )
-    //     const QuantityUpdate =
-
-    //         await Product.updateOne({
-    //             _id: findCartItem?.product[0]?._id,
-    //             "sizesAndColor._id": updateQuantityId[0]?._id,
-    //         }, {
-    //             $set: {
-    //                 'sizesAndColor.$.quantity': updateQuantityId[0]?.quantity - findCartItem?.quantity,
-    //             }
-    //         })
-    //     await product.save();
-
-    //     const del = await deleteCartItem(id)
-    // }
-    // await Cart.findByIdAndDelete(cartId)
-
-    if (savedOrder) {
-        return "Order Created Successfully";
-    }
 }
 
 
